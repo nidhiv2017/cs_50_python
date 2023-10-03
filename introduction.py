bed219832c6f1ff78370d8f29dbae2ec8d4902d1
nidhiv2017 <116021984+nidhiv2017@users.noreply.github.com>
2023-10-03 18:38:54 +0000

    initiated introduction.py

---
+++ introduction.py
@@ -0,0 +1,13 @@
+# Introduction to programming with python
+# 1. Function, Variables
+# 2. Conditionals
+# 3. Loops
+# 4. Exceptions
+# 5. Libraries
+# 6. Unit Tests
+# 7. File I/O
+# 8. Regular Expressions
+# 9. Object-Oriented Programming
+# 10. Et Cetera
+# By David J. Malan
+# malan@harvard.edu
