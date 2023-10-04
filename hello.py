8d48691f77b3604f35aaaef6c8d1a0cb882a3175
nidhiv2017 <116021984+nidhiv2017@users.noreply.github.com>
2023-10-04 01:58:50 +0000

    variable and pseudocode

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print("Hello, World !")
+# comments, pseudocode, return values and variables
+
+# Ask user for their name
+name = input("What's your name ? ")
+
+# Say hello to user
+print("Hello,")
+print(name)
+
+# arguments, "hello, world (input) is an argument for print function"
