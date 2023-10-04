ecb366e052344641a48bfe77ac54610eccb6fd18
nidhiv2017 <116021984+nidhiv2017@users.noreply.github.com>
2023-10-04 17:27:31 +0000

    multi-line comment

--- hello.py
+++ hello.py
@@ -6,5 +6,9 @@ name = input("What's your name ? ")
 # Say hello to user
 print("Hello,")
 print(name)
+"""
+This is a multiline comment
+potatoes and tomatoes
+"""
 
 # arguments, "hello, world (input) is an argument for print function"
