9930e84830e830398febc1b6f86797bd90865203
nidhiv2017 <116021984+nidhiv2017@users.noreply.github.com>
2023-10-06 20:26:44 +0000

    Made commit for strip and title

---
+++ pre7october_hello.py
@@ -0,0 +1,40 @@
+# comments, pseudocode, return values and variables
+
+# Ask user for their name
+name = input("What's your name ? ")
+
+# Say hello to user
+# print("Hello, " + name + name, name)
+# print("Hello,",name)
+
+# print("Hello,", end=" ")
+# print(name)
+# print("Hello,",name, sep= " balls ")
+# positional parameter sep and end
+# named parameter sep and end
+# print("hello, "friend"")
+# print('hello, "friend"')
+# print('hello, \"friend\"')
+# backslash is an escape character
+# print("hello, \"friend\"")
+# -----------------------------------
+
+# Remove whitespace from str
+# name = name.strip()
+
+
+# Capitalize user name
+# name = name.capitalize()
+
+# # say hello to user
+# print(f"hello, {name}")
+
+#  Title of user name
+name = name.capitalize()
+print(f"hello, {name}")
+'''
+This is a multiline comment
+potatoes and tomatoes
+'''
+
+# arguments, "hello, world (input) is an argument for print function"
