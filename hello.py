2954a3b7e851b5e68aaaf95e73577c2b648128a3
nidhiv2017 <116021984+nidhiv2017@users.noreply.github.com>
2023-10-07 18:14:17 +0000

    split by space by first and last name

--- hello.py
+++ hello.py
@@ -1,3 +1,8 @@
-name = input("What's your name ?").strip().title()
+# Ask user for their name
+name = input("What's your name ? ").strip().title()
 
-print(f"Hello, {name}") 
+# Split user's name into first name and last name 
+first, last = name.split(" ")
+
+# Say hello to user
+print(f"Hello, {first}") 
